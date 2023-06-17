1887ae89b221daaa9fc2442c76e8f448e854469f
konstantinVolodko <volodko2000@bk.ru>
2023-06-17 20:06:55 +0300

    hosting

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,8 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
     //= components/
 
 
-    
+    let ourTeachersSwiper = new Swiper(".ourTeachersSwiper", {
+        slidesPerView: 4.3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".ourTeachers-content__rightArrow",
+            prevEl: ".ourTeachers-content__leftArrow",
+        },
+    });
 
+    var reviewsSwiper = new Swiper(".reviewsSwiper", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        pagination: {
+            el: ".reviewsSwiper-pagination",
+        },
+    });
 })
 
 
